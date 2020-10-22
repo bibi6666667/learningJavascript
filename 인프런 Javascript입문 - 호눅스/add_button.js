74eff30d5ce62756e9c6416478988a72707d8b0a
bibi6666667 <bibi6666667@kakao.com>
2020-10-22 19:31:35 +0900

    [인프런] Javascript입문- DOM과 input, button 예제

---
+++ 인프런 Javascript입문 - 호눅스/add_button.js
@@ -0,0 +1,13 @@
+console.log("addButton.js");
+
+var readInput = function() {
+    var input = document.getElementById("input1");
+    console.log(input.value);
+}
+
+var btn2 = document.createElement("button");
+btn2.innerHTML = "버튼2";
+btn2.onclick = readInput;
+var test = document.getElementById("test");
+test.appendChild(document.createElement('br'));
+test.appendChild(btn2)
